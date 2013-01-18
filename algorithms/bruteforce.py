b1cb118fbc8e6cccb2aa3a17bf5d222d3449df1b
Błażej Kotowski <blaziko@gmail.com>
2013-01-18 14:01:01 +0100

    bruteforce algorithm and simulation

--- algorithms/bruteforce.py
+++ algorithms/bruteforce.py
@@ -1,3 +1,40 @@
 import sys
 sys.path.append('..')
 from base import *
+
+cycles = []
+
+def find_cycles(graph, node=0, path=[], cost=0):
+    path.append(node)
+    
+    if len(path) > 1:
+        cost += graph.cost(path[-2], node)
+    
+    if len(path) == graph.size and graph.is_adjacent(node, path[0]):
+        path.append(path[0])
+        cost += graph.cost(node, path[0])
+        global cycles
+        cycles.append((cost, path,))
+    else:
+        for u in graph.adjacency_list(node).iterkeys():
+            if u not in path:
+                find_cycles(graph, u, list(path), cost)
+            
+            
+def bruteforce(graph, start=0):
+    global cycles
+    find_cycles(graph, start)
+    cycles.sort()
+    try:
+        return cycles[0]
+    except:
+        return (False, False)
+        
+if __name__ == '__main__':
+    print "Result for random 5-vertex complete graph"
+    graph = Graph.complete(5)
+    cost, path = bruteforce(graph)
+    if cost is not False:
+        print "Cheapest path costs %d and constists of: %s" % (cost, ", ".join(map(str,path)))
+    else:
+        print "There is no Hamillton cycle in given graph"
